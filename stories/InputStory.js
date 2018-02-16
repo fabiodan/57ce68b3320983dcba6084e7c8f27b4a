b998565a0885f8f85b87b7fb932d0e56e2f235a0
Chandler Van De Water <chandler.vdw@walmart.com>
2018-02-16 09:22:26 -0500

    Fixes overlay

--- stories/InputStory.js
+++ stories/InputStory.js
@@ -8,31 +8,6 @@ import styled from 'styled-components';
 import Input from '../components/Input';
 import Textarea from '../components/Textarea';
 
-const BGImage = styled.div`
-  border-radius: 8px;
-  overflow: hidden;
-  background: url('https://be35832fa5168a30acd6-5c7e0f2623ae37b4a933167fe83d71b5.ssl.cf3.rackcdn.com/468/mini-yorkshire-puddings-with-beef__hero.jpg') no-repeat;
-  background-size: cover;
-  background-position-x: center;
-  width: 800px;
-  height: 500px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  &:before {
-    content: '';
-    display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background: rgba(0,0,0,.2);
-    border-radius: 8px;
-  }
-`;
-
 const imageUrl = '../images/creditcards';
 const iconOptions = [
   '',
