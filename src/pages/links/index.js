b2241511c596faf3967e8b7b89cda2e2012cf24d
Fabio Dan <fcardoso@walmartlabs.com>
2019-03-23 14:20:01 -0700

    Add Links section for Links page

--- src/pages/links/index.js
+++ src/pages/links/index.js
@@ -3,10 +3,18 @@ import React from 'react'
 
 // Components (from atomic to composite)
 import { LinkButton, LinkAnchor } from '@asda/link'
-import { Figure, Paragraph, Subheading, Section, Heading } from '../components/sg-layout'
+import { Caption, Figure, Paragraph, Subheading, Section, Heading } from '../components/sg-layout'
 
 // Assets
 import './_style.scss'
+
+const Grid = ({ children }) => (
+  <ul className="links-page__grid">{children}</ul>
+)
+
+const GridItem = ({ children }) => (
+  <li className="links-page__grid-item">{children}</li>
+)
 
 const LinksPage = () => (
   <article className="links-page">
@@ -20,15 +28,29 @@ const LinksPage = () => (
         types available, standalone buttons and text links which are part of
         other text bodies.
       </Paragraph>
-
-      <LinkButton align="left">link button</LinkButton>
-      <LinkButton>link button</LinkButton>
-      <LinkButton align="right">link button</LinkButton>
-
-      <LinkButton className="links-page__link--border" align="left">link button</LinkButton>
-      <LinkButton className="links-page__link--border">link button</LinkButton>
-      <LinkButton className="links-page__link--border" align="right">link button</LinkButton>
-
+      <Grid>
+        <GridItem>
+          <LinkButton align="left">link button</LinkButton>
+          <Caption>Standalone button Left aligned Default</Caption>
+        </GridItem>
+        <GridItem>
+          <LinkButton>link button</LinkButton>
+          <Caption>standalone button Center aligned Default</Caption>
+        </GridItem>
+        <GridItem>
+          <LinkButton align="right">link button</LinkButton>
+          <Caption>standalone button Right aligned Hover</Caption>
+        </GridItem>
+        <GridItem>
+          <LinkButton className="links-page__link--border" align="left">link button</LinkButton>
+        </GridItem>
+        <GridItem>
+          <LinkButton className="links-page__link--border">link button</LinkButton>
+        </GridItem>
+        <GridItem>
+          <LinkButton className="links-page__link--border" align="right">link button</LinkButton>
+        </GridItem>
+      </Grid>
       <Paragraph>
         Tap areas for links are larger than just the text area. Height for tap
         area is 40px and width is 8-16px on each side, based on the text
