0812088d4a9d29b55079aa18840635757bd4b5cd
Fabio Cardoso - fcardo3 <fcardoso@walmartlabs.com>
2019-06-17 15:46:24 -0700

    Implement modal example (#7)

---
+++ src/components/modal/index.js
@@ -0,0 +1,163 @@
+// Libs
+import React, { Component } from 'react'
+import joinClassNames from 'classnames'
+
+// Components (from atomic to composite)
+import { IconButton } from '@asda/icon'
+import Button from '@asda/button'
+import FocusLock from 'react-focus-lock';
+
+// Assets
+import './_style.scss'
+
+class Modal extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleEscKeydown = this.handleEscKeydown.bind(this)
+  }
+
+  handleEscKeydown(e) {
+    const { onEscKeydown } = this.props
+
+    if (e.keyCode === 27) onEscKeydown()
+  }
+
+  componentDidMount() {
+    const { onEscKeydown } = this.props
+
+    document.body.classList.add('modal--scroll-lock')
+
+    if (onEscKeydown) {
+      document.addEventListener('keydown', this.handleEscKeydown, false)
+    }
+  }
+
+  componentWillUnmount() {
+    const { onEscKeydown } = this.props
+
+    document.body.classList.remove('modal--scroll-lock')
+
+    if (onEscKeydown) {
+      document.removeEventListener('keydown', this.handleEscKeydown, false)
+    }
+  }
+
+  render() {
+    const {
+      children,
+      className,
+      scrollable,
+      size,
+    } = this.props
+
+    const modifiers = [
+      scrollable && 'modal--scrollable',
+      !!size && `modal--size-${size}`,
+    ]
+    const classNames = joinClassNames('modal', className, modifiers)
+
+    return (
+      <div className={classNames} role="modal" aria-modal="true">
+        { children }
+      </div>
+    )
+  }
+}
+
+const ModalBackdrop = ({ children, className, onClick }) => {
+  const modifiers = []
+  const classNames = joinClassNames('modal__backdrop', className, modifiers)
+
+  return (
+    <div className={classNames} onClick={onClick}>
+      { children }
+    </div>
+  )
+}
+
+const ModalHeader = ({ children, className }) => {
+  const modifiers = []
+  const classNames = joinClassNames('modal__header', className, modifiers)
+
+  return (
+    <h2 className={classNames}>
+      { children }
+    </h2>
+  )
+}
+
+const ModalContainer = ({ children, className }) => {
+  const modifiers = []
+  const classNames = joinClassNames('modal__container', className, modifiers)
+
+  return (
+<FocusLock>
+    // modal__outer-container is needed to fix scroll issues on IE11 :P
+    <div className="modal__outer-container">
+      <div className={classNames}>
+        { children }
+      </div>
+    </div>
+    </FocusLock>
+  )
+}
+
+const ModalContent = ({ children, className }) => {
+  const modifiers = []
+  const classNames = joinClassNames('modal__content', className, modifiers)
+
+  return (
+    <section className={classNames}>
+      { children }
+    </section>
+  )
+}
+
+const ModalActionBar = ({ children, className }) => {
+  const modifiers = []
+  const classNames = joinClassNames('modal__action-bar', className, modifiers)
+
+  return (
+    <footer className={classNames}>
+      { children }
+    </footer>
+  )
+}
+
+const ModalButton = ({ children, className, ...rest }) => {
+  const modifiers = []
+  const classNames = joinClassNames('modal__button', className, modifiers)
+
+  return (
+    <Button className={classNames} {...rest}>
+      { children }
+    </Button>
+  )
+}
+
+
+const ModalCloseIcon = ({ className, onClick }) => {
+  const modifiers = []
+  const classNames = joinClassNames('modal__close-icon', className, modifiers)
+
+  return (
+    <IconButton
+      className={classNames}
+      name="close"
+      size="small"
+      onClick={onClick}
+    />
+  )
+}
+
+export {
+  Modal as default,
+  ModalContainer,
+  ModalHeader,
+  ModalContent,
+  ModalActionBar,
+  ModalButton,
+  ModalCloseIcon,
+  ModalBackdrop,
+}
--- src/pages/modals/index.js
+++ src/pages/modals/index.js
@@ -1,11 +1,23 @@
 // Libs
-import React from 'react'
+import React, { Component } from 'react'
 
 // Components (from atomic to composite)
+import { IconButton } from '@asda/icon'
+import Button from '@asda/button'
+import Modal, {
+  ModalHeader,
+  ModalContent,
+  ModalActionBar,
+  ModalContainer,
+  ModalBackdrop,
+  ModalButton,
+  ModalCloseIcon,
+} from '@asda/modal'
+
 import { Figure, Paragraph, Subheading, Section, Heading } from '../components/sg-layout'
 
 // Assets
-import dialogSVGSource from '../../assets/images/modals-page-dialog.svg'
+import modalSVGSource from '../../assets/images/modals-page-modal.svg'
 import infoModal768SVGSource from '../../assets/images/modals-page-info-modal-768.svg'
 import infoModal360SVGSource from '../../assets/images/modals-page-info-modal-360.svg'
 import infoModal360WithIconSVGSource from '../../assets/images/modals-page-info-modal-360-with-icon.svg'
@@ -16,131 +28,188 @@ import paddingModal768SVGSource from '../../assets/images/modals-page-padding-mo
 import paddingModal360SVGSource from '../../assets/images/modals-page-padding-modal-360.svg'
 import './_style.scss'
 
-const ModalsPage = () => (
-  <article className="modals-page">
+class ModalsPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showModal: true,
+    }
 
-    <Heading>Modals</Heading>
+    this.closeModal = this.closeModal.bind(this)
+  }
 
-    <Section>
-      <Subheading>Definition</Subheading>
-      <Paragraph>
-        A modal window is an element that sits on top. It creates a mode that
-        disables the main window but keeps it visible with the modal window as
-        a child window in front of it. Users must interact with the modal window
-        before they can return to the parent application.
-      </Paragraph>
-      <Paragraph noMargin>
-        Modals are purposefully interruptive and should be used sparingly.
-      </Paragraph>
-    </Section>
+  closeModal() {
+    this.setState({ showModal: false })
+  }
 
-    <Section>
-      <Subheading>Dialog</Subheading>
-      <Paragraph>
-        A dialog is a type of modal window that provides critical information or
-        asks for a decision before user can continue with the journey.
-      </Paragraph>
-      <Figure
-        maxWidth="772px"
-        src={dialogSVGSource}
-        caption="Slot reserved Dialog (768PX)"
-        noMargin
-      />
-    </Section>
+  render() {
+    return (
+      <article className="modals-page">
 
-    <Section>
-      <Subheading>Informational Modal</Subheading>
-      <Paragraph>
-        These modals appear in response to an user action, with relevant or
-        contextual information. Titles should contain be brief and contain a
-        concise statement or a questions explaining the purpose of the modal.
-      </Paragraph>
-      <Paragraph>
-        Use the grid to control the width of the modal, and the content for the
-        height of the modal (middle align the entire modal to the screen).
-        Ideally, the content should be short and digestible, avoid long content
-        as the modal will interupt the user journey.
-      </Paragraph>
-      <Figure
-        maxWidth="768px"
-        src={infoModal768SVGSource}
-        caption="Informational Modal (768px)"
-      />
-      <div className="modals-page__img-container">
-        <Figure
-          className="modals-page__figure"
-          maxWidth="360px"
-          src={infoModal360SVGSource}
-          caption="Informational Modal (360px)"
-        />
-        <Figure
-          maxWidth="360px"
-          src={infoModal360WithIconSVGSource}
-          caption="Informational Modal with icon (360px)"
-          noMargin
-        />
-      </div>
-    </Section>
+        <Heading>Modals</Heading>
 
-    <Section>
-      <Subheading>Scrollable Modal</Subheading>
-      <Paragraph>
-        Modals where the content is more than the height of the screen, use
-        scrollable version. Maintain substantial padding around the modal to
-        remind the user that he is a modal, while giving him enough space to
-        perform the tasks.
-      </Paragraph>
-      <Figure
-        maxWidth="768px"
-        src={scrollableModal768SVGSource}
-        caption="Scrollable Modal (768px)"
-      />
-      <Figure
-        maxWidth="768px"
-        src={scrollableModal360SVGSource}
-        caption="Scrollable Modal (360px)"
-        noMargin
-      />
-    </Section>
+        <Section>
+          <Subheading>Definition</Subheading>
+          <Paragraph>
+            A modal window is an element that sits on top. It creates a mode that
+            disables the main window but keeps it visible with the modal window as
+            a child window in front of it. Users must interact with the modal window
+            before they can return to the parent application.
+          </Paragraph>
+          <Paragraph noMargin>
+            Modals are purposefully interruptive and should be used sparingly.
+          </Paragraph>
 
-    <Section>
-      <Subheading>Avoid Links in Modal</Subheading>
-      <Paragraph>
-        Most modal content should avoid links or actions which navigate away
-        from the modal, creating further friction in the user journey.
-      </Paragraph>
-      <Paragraph>
-        In situations when links are neccessary, open a new tab in the
-        background while leaving the modal and the user on the current screen.
-      </Paragraph>
+          {this.state.showModal &&
+            <Modal onEscKeydown={this.closeModal}>
+              <ModalContainer>
+                <ModalHeader>
+                  Header
+                </ModalHeader>
+                <ModalContent>
+                  <a href="#">Lorem ipsum dolor sit amet</a>, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus velit quibusdam nulla sint ea tenetur ex corporis, eaque rem, quaerat quo consectetur nostrum consequatur culpa aperiam distinctio non quae commodi!
+                </ModalContent>
+                <ModalActionBar>
+                  <ModalButton category="secondary" onClick={this.closeModal}>Cancel</ModalButton>
+                  <ModalButton onClick={this.closeModal}>Continue</ModalButton>
+                </ModalActionBar>
+                <ModalCloseIcon onClick={this.closeModal} />
+              </ModalContainer>
+              <ModalBackdrop onClick={this.closeModal} />
+            </Modal>
+          }
+        </Section>
 
-      <Figure
-        maxWidth="768px"
-        src={avoidLinksSVGSource}
-        noMargin
-      />
-    </Section>
+        <Section>
+          <Subheading>Modal</Subheading>
+          <Paragraph>
+            A modal is a type of modal window that provides critical information or
+            asks for a decision before user can continue with the journey.
+          </Paragraph>
+          <Figure
+            maxWidth="772px"
+            src={modalSVGSource}
+            caption="Slot reserved Modal (768PX)"
+            noMargin
+          />
+        </Section>
 
-    <Section>
-      <Subheading>Padding in Modals</Subheading>
-      <Paragraph>
-        For screens above 768, use 32px all around margin inside in the modal.
-        For smaller size, 24px all around margin should be maintained, for case
-        with limited space, the side margins can be reduced to 16px.
-      </Paragraph>
-      <Figure
-        maxWidth="798px"
-        src={paddingModal768SVGSource}
-        caption="Informational Modal (768px)"
-      />
-      <Figure
-        maxWidth="387px"
-        src={paddingModal360SVGSource}
-        caption="Informational Modal (360px)"
-        noMargin
-      />
-    </Section>
-  </article>
-)
+        <Section>
+          <Subheading>Informational Modal</Subheading>
+          <Paragraph>
+            These modals appear in response to an user action, with relevant or
+            contextual information. Titles should contain be brief and contain a
+            concise statement or a questions explaining the purpose of the modal.
+          </Paragraph>
+          <Paragraph>
+            Use the grid to control the width of the modal, and the content for the
+            height of the modal (middle align the entire modal to the screen).
+            Ideally, the content should be short and digestible, avoid long content
+            as the modal will interupt the user journey.
+          </Paragraph>
+          <Figure
+            maxWidth="768px"
+            src={infoModal768SVGSource}
+            caption="Informational Modal (768px)"
+          />
+          <div className="modals-page__img-container">
+            <Figure
+              className="modals-page__figure"
+              maxWidth="360px"
+              src={infoModal360SVGSource}
+              caption="Informational Modal (360px)"
+            />
+            <Figure
+              maxWidth="360px"
+              src={infoModal360WithIconSVGSource}
+              caption="Informational Modal with icon (360px)"
+              noMargin
+            />
+          </div>
+        </Section>
+
+        <Section>
+          <Subheading>Scrollable Modal</Subheading>
+          <Paragraph>
+            Modals where the content is more than the height of the screen, use
+            scrollable version. Maintain substantial padding around the modal to
+            remind the user that he is a modal, while giving him enough space to
+            perform the tasks.
+          </Paragraph>
+          <Figure
+            maxWidth="768px"
+            src={scrollableModal768SVGSource}
+            caption="Scrollable Modal (768px)"
+          />
+          <Figure
+            maxWidth="768px"
+            src={scrollableModal360SVGSource}
+            caption="Scrollable Modal (360px)"
+            noMargin
+          />
+        </Section>
+
+        <Section>
+          <Subheading>Avoid Links in Modal</Subheading>
+          <Paragraph>
+            Most modal content should avoid links or actions which navigate away
+            from the modal, creating further friction in the user journey.
+          </Paragraph>
+          <Paragraph>
+            In situations when links are neccessary, open a new tab in the
+            background while leaving the modal and the user on the current screen.
+          </Paragraph>
+
+          <Figure
+            maxWidth="768px"
+            src={avoidLinksSVGSource}
+            noMargin
+          />
+        </Section>
+
+        <Section>
+          <Subheading>Padding in Modals</Subheading>
+          <Paragraph>
+            For screens above 768, use 32px all around margin inside in the modal.
+            For smaller size, 24px all around margin should be maintained, for case
+            with limited space, the side margins can be reduced to 16px.
+          </Paragraph>
+          <Figure
+            maxWidth="798px"
+            src={paddingModal768SVGSource}
+            caption="Informational Modal (768px)"
+          />
+          <Figure
+            maxWidth="387px"
+            src={paddingModal360SVGSource}
+            caption="Informational Modal (360px)"
+            noMargin
+          />
+        </Section>
+      </article>
+    )
+  }
+}
 
 export default ModalsPage
