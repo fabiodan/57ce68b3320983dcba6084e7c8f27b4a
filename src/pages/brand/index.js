75dc0f2ee10fa2d03fe26d6cee16fbfa8947c35b
Fabio Dan <fcardoso@walmartlabs.com>
2019-08-16 14:10:38 -0700

    Update CreditCard component. Update Brand Elements page.

--- src/pages/brand/index.js
+++ src/pages/brand/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 
 // Components (from atomic to composite)
 import ExpressLogo from '@asda/brand/express-logo'
+import CreditCardIcon from '@asda/credit-card-icon'
 import { Caption, Paragraph, Subheading, Section, Heading } from '../components/sg-layout'
 
 // Assets
@@ -24,7 +25,7 @@ const BrandPage = () => (
       <Grid>
         <GridItem>
           <Caption position="top">Large</Caption>
-          <ExpressLogo size="large"/>
+          <ExpressLogo/>
         </GridItem>
         <GridItem>
           <Caption position="top">Medium</Caption>
@@ -33,6 +34,68 @@ const BrandPage = () => (
         <GridItem>
           <Caption position="top">Small</Caption>
           <ExpressLogo size="small"/>
+        </GridItem>
+      </Grid>
+    </Section>
+
+    <Section>
+      <Subheading>Credit Cards</Subheading>
+      <Grid>
+        <GridItem>
+          <Caption position="top">Add New Card</Caption>
+          <CreditCardIcon name="add-new" />
+        </GridItem>
+        <GridItem>
+          <Caption position="top">Blank</Caption>
+          <CreditCardIcon name="blank" />
+        </GridItem>
+        <GridItem>
+          <Caption position="top">Amex</Caption>
+          <CreditCardIcon name="amex" />
+        </GridItem>
+        <GridItem>
+          <Caption position="top">CVV</Caption>
+          <CreditCardIcon name="cvv" />
+        </GridItem>
+        <GridItem>
+          <Caption position="top">Gift Card Code</Caption>
+          <CreditCardIcon name="gift-code" />
+        </GridItem>
+        <GridItem>
+          <Caption position="top">Cirrus</Caption>
+          <CreditCardIcon name="cirrus" />
+        </GridItem>
+        <GridItem>
+          <Caption position="top">Diners</Caption>
+          <CreditCardIcon name="diners" />
+        </GridItem>
+        <GridItem>
+          <Caption position="top">Discover</Caption>
+          <CreditCardIcon name="discover" />
+        </GridItem>
+        <GridItem>
+          <Caption position="top">Maestro</Caption>
+          <CreditCardIcon name="maestro" />
+        </GridItem>
+        <GridItem>
+          <Caption position="top">Mastercard</Caption>
+          <CreditCardIcon name="mastercard" />
+        </GridItem>
+        <GridItem>
+          <Caption position="top">Plus</Caption>
+          <CreditCardIcon name="plus" />
+        </GridItem>
+        <GridItem>
+          <Caption position="top">Visa</Caption>
+          <CreditCardIcon name="visa" />
+        </GridItem>
+        <GridItem>
+          <Caption position="top">ASDA Gift Cards</Caption>
+          <CreditCardIcon name="gift" />
+        </GridItem>
+        <GridItem>
+          <Caption position="top">Xmas Gift Cards</Caption>
+          <CreditCardIcon name="xmas" />
         </GridItem>
       </Grid>
     </Section>
