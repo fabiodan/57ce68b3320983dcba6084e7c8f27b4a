783ed82c08a32f0b22c812feb166d05c242031ec
Fabio Dan <fcardoso@walmartlabs.com>
2019-11-05 15:30:42 -0800

    Improve shell script

--- copy-to-qa.sh
+++ copy-to-qa.sh
@@ -1,7 +1,10 @@
 echo Deleting .DS_Store files... &&
-./rm-destore.sh
+sh rm-dstore.sh &&
+echo
 echo Deleting /assets/ folder from www.asda.com-static-assets-stage/design/... &&
 rm -rf ~/Projects/www.asda.com-static-assets-stage/design/assets/ &&
+echo Assets deleted
+echo
 echo Copying /dist/ folder to www.asda.com-static-assets-stage/design/... &&
 cp -R ~/Projects/asda-design-system/dist/ ~/Projects/www.asda.com-static-assets-stage/design/ &&
 echo Done
