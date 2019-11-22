d1946b1ef3c15bc4bddb37c104dc104bfe766afb
Xu Jing - vn0hban <Xu.Jing@walmartlabs.com>
2019-11-22 14:32:19 -0800

    Add pickers page from iOS (#18)
    * Add pickers page from iOS
    
    * Remove .txt file under the iOS pickers page
    
    * Remove .txt file under the iOS pickers page and change the import order

--- src/pages/ios/pickers/index.js
+++ src/pages/ios/pickers/index.js
@@ -1,32 +1,70 @@
 // Libs
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 // Components (from atomic to composite)
-import { Figure, FigureContainer, Caption, Paragraph, Heading2, Section, Heading1,} from '@sg/sg-layout'
+import {
+  Heading2,
+  Heading1,
+  Caption,
+  Figure,
+  FigureContainer,
+  GridItem,
+  Grid,
+  Paragraph,
+  Section,
+} from '@sg/sg-layout'
 
 // Assets
+import dateandtimePNGSource from './images/pickers-date-and-time.png'
+import datePNGSource from './images/pickers-date.png'
 import examplePNGSource from './images/pickers-example.png'
+import numericPNGSource from './images/pickers-numeric.png'
+import timePNGSource from './images/pickers-time.png'
 
 import './_style.scss'
 
 
-const IOSPickersPage = () => (
-  <article className="ios-pickers-page">
+const IOSPickersPage = () => {
+  const minWidth1024 = useMediaQuery({ query: '(min-width: 1024px)' })
+  return (
+    <article className="ios-pickers-page">
 
-    <Heading1>Pickers</Heading1>
+      <Heading1>Pickers</Heading1>
 
-    <Section>
-      <Heading2>Pickers</Heading2>
-      <Paragraph>
-        Pickers allow users to select a value from a list. Native pickers on iOS
-        are best suited for a small number of values.
-      </Paragraph>
-      <FigureContainer>
-        <Figure noMargin fluid maxWidth="350px" src={examplePNGSource} />
-      </FigureContainer>
-    </Section>
+      <Section>
+        <Heading2>Pickers</Heading2>
+        <Paragraph>
+          Pickers allow users to select a value from a list. Native pickers on iOS
+          are best suited for a small number of values.
+        </Paragraph>
+        <Grid columnCount={ minWidth1024 ? 2 : 1 }>
+          <GridItem>
+            <Caption position="top">Numeric Picker</Caption>
+            <Figure maxWidth="375px" src={numericPNGSource} noMargin fluid />
+          </GridItem>
+          <GridItem>
+            <Caption position="top">Date Picker</Caption>
+            <Figure maxWidth="375px" src={datePNGSource} noMargin fluid />
+          </GridItem>
+          <GridItem>
+            <Caption position="top">Date & Time Picker</Caption>
+            <Figure maxWidth="375px" src={dateandtimePNGSource} noMargin fluid />
+          </GridItem>
+          <GridItem>
+            <Caption position="top">Time Picker</Caption>
+            <Figure maxWidth="375px" src={timePNGSource} noMargin fluid />
+          </GridItem>
+        </Grid>
+        <Grid withBackground noMargin>
+          <GridItem>
+            <Figure maxWidth="223px" src={examplePNGSource} noMargin fluid />
+          </GridItem>
+        </Grid>
+      </Section>
 
-  </article>
-)
+    </article>
+  )
+}
 
 export default IOSPickersPage
