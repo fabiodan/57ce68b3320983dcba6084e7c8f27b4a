d24fce669324d65b5b9a72fcc9194809dc65e42a
Xu Jing - vn0hban <Xu.Jing@walmartlabs.com>
2020-01-06 13:49:54 -0800

    Update the Action-Sheets-Page for iOS (#25)

--- src/pages/ios/action-sheets/index.js
+++ src/pages/ios/action-sheets/index.js
@@ -1,35 +1,54 @@
 // Libs
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 // Components (from atomic to composite)
-import { Figure, Caption, Paragraph, Heading2, Section, Heading1 } from '@sg/sg-layout'
+import {
+  Caption,
+  Paragraph,
+  Heading1,
+  Heading2,
+  Figure,
+  GridItem,
+  Grid,
+  Section,
+} from '@sg/sg-layout'
 
 // Assets
 import devicePNGSource from './images/action-sheets-device.png'
 import standardPNGSource from './images/action-sheets-standard.png'
 import './_style.scss'
 
-const IOSActionSheetsPage = () => (
-  <article className="ios-action-sheets-page">
-    <Heading1>Action Sheets</Heading1>
+const IOSActionSheetsPage = () => {
+  const minWidth1024 = useMediaQuery({ query: '(min-width: 1024px)' })
 
-    <Section>
-      <Heading2>Action sheets</Heading2>
-      <Paragraph>
-        An action sheet is an overlay that appears in response to a control or
-        action - presenting a set of two or more choices based on the current context.
-      </Paragraph>
+  return (
+    <article className="ios-action-sheets-page">
+      <Heading1>Action Sheets</Heading1>
 
-      <div className="ios-action-sheets-page__examples">
-        <div>
-          <Caption position="top">Standard Action Sheet with different actions</Caption>
-          <Figure fluid maxWidth="375px" src={standardPNGSource} />
-        </div>
-        <Figure className="ios-action-sheets-page__figure" noMargin fluid maxWidth="375px" src={devicePNGSource} />
-      </div>
-    </Section>
+      <Section>
+        <Heading2>Action sheets</Heading2>
+        <Paragraph>
+          An action sheet is an overlay that appears in response to a control or
+          action - presenting a set of two or more choices based on the current context.
+        </Paragraph>
 
-  </article>
-)
+        <Grid withBackground>
+          <GridItem>
+            <Caption position="top">Standard Action Sheet with different actions</Caption>
+            <Figure noMargin fluid maxWidth="375px" src={standardPNGSource} />
+          </GridItem>
+        </Grid>
+
+        <Grid withBackground>
+          <GridItem>
+            <Figure noMargin fluid maxWidth="223px" src={devicePNGSource} />
+          </GridItem>
+        </Grid>
+      </Section>
+
+    </article>
+  )
+}
 
 export default IOSActionSheetsPage
