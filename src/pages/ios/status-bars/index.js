7a6277d5c3d358e5fdbd0d515df1f6f02dd36afd
Xu Jing - vn0hban <Xu.Jing@walmartlabs.com>
2020-01-06 13:29:14 -0800

    Update the Status-Bars-Page for iOS (#23)

--- src/pages/ios/status-bars/index.js
+++ src/pages/ios/status-bars/index.js
@@ -1,64 +1,78 @@
 // Libs
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 // Components (from atomic to composite)
-import { Figure, Caption, Paragraph, Heading2, Section, Heading1 } from '@sg/sg-layout'
+import {
+  Caption,
+  Paragraph,
+  Heading1,
+  Heading2,
+  Figure,
+  GridItem,
+  Grid,
+  Section,
+} from '@sg/sg-layout'
 
 // Assets
-import exampleIOSPNGSource from './images/status-bar-example-ios.png'
-import largeDarkPNGSource from './images/status-bar-large-dark.png'
-import largeLightPNGSource from './images/status-bar-large-light.png'
-import smallDarkPNGSource from './images/status-bar-small-dark.png'
-import smallLightPNGSource from './images/status-bar-small-light.png'
+import largePNGSource from './images/status-bar-large.png'
+import smallPNGSource from './images/status-bar-small.png'
 import './_style.scss'
 
-const IOSStatusBarsPage = () => (
-  <article className="ios-status-bars-page">
-    <Heading1>Status bars</Heading1>
+const IOSStatusBarsPage = () => {
+  const minWidth1024 = useMediaQuery({ query: '(min-width: 1024px)' })
 
-    <Section>
-      <Heading2>Status Bars</Heading2>
-      <Paragraph>
-        The status bar appears on the top of the screen and shows information
-        such as time, battery percentage, network and cellular status.
-      </Paragraph>
-      <Paragraph>
-        If the device has a notch at the top of the screen, the status bar looks
-        slightly different and is taller.
-      </Paragraph>
-      <Paragraph>
-        Depending on the background colour of the app navigation bar, the status
-        bar can be shown with black text (light status bar) or white text
-        (dark status bar).
-      </Paragraph>
-      <Paragraph>
-        The status bar can also be hidden when interacting with full screen pages.
-      </Paragraph>
+  return (
+    <article className="ios-status-bars-page">
+      <Heading1>Status bars</Heading1>
 
-      <div className="ios-status-bars-page__examples">
-        <ul className="ios-status-bars-page__list">
-          <li className="ios-status-bars-page__list-item">
-            <Caption position="top">Large status bar - Light</Caption>
-            <Figure noMargin fluid maxWidth="375px" src={largeLightPNGSource} />
-          </li>
-          <li className="ios-status-bars-page__list-item">
-            <Caption position="top">Large status bar - Dark</Caption>
-            <Figure noMargin fluid maxWidth="375px" src={largeDarkPNGSource} />
-          </li>
-          <li className="ios-status-bars-page__list-item">
-            <Caption position="top">Small status bar - Light</Caption>
-            <Figure noMargin fluid maxWidth="375px" src={smallLightPNGSource} />
-          </li>
-          <li className="ios-status-bars-page__list-item">
-            <Caption position="top">Small status bar - Dark</Caption>
-            <Figure noMargin fluid maxWidth="375px" src={smallDarkPNGSource} />
-          </li>
-        </ul>
-        <Figure className="ios-status-bars-page__figure" fluid maxWidth="356px" src={exampleIOSPNGSource} />
-      </div>
-    </Section>
+      <Section>
+        <Heading2>Status Bars</Heading2>
+        <Paragraph>
+          The status bar appears on the top of the screen and shows information
+          such as time, battery percentage, network and cellular status.
+        </Paragraph>
+        <Paragraph>
+          If the device has a notch at the top of the screen, the status bar looks
+          slightly different and is taller.
+        </Paragraph>
+        <Paragraph>
+          Depending on the background colour of the app navigation bar, the status
+          bar can be shown with black text (light status bar) or white text
+          (dark status bar).
+        </Paragraph>
+        <Paragraph>
+          The status bar can also be hidden when interacting with full screen pages.
+        </Paragraph>
+      </Section>
 
-  </article>
-)
+      <Section>
+        <Heading2>iPhone SE, 8, 8 Plus & iPad</Heading2>
+        <Paragraph>
+          These devices use the <b>SMALLER</b> status bar, shown below.
+        </Paragraph>
+        <Grid className="ios-status-bars-page_grid" withBackground>
+          <GridItem>
+            <Figure noMargin fluid maxWidth="806px" src={smallPNGSource}/>
+          </GridItem>
+        </Grid>
+      </Section>
+
+      <Section>
+        <Heading2>iPhone 11 Pro, iPhone 11 Pro Max</Heading2>
+        <Paragraph>
+          These devices use the <b>LARGER</b> status bar, shown below, as the device
+          has a notch at the top of the screen.
+        </Paragraph>
+        <Grid className="ios-status-bars-page_grid" withBackground>
+          <GridItem>
+            <Figure noMargin fluid maxWidth="806px" src={largePNGSource}/>
+          </GridItem>
+        </Grid>
+      </Section>
+
+    </article>
+  )
+}
 
 export default IOSStatusBarsPage
