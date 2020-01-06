6c222ba07a5b8be174c52826ce94ad4bf57148a7
Xu Jing - vn0hban <Xu.Jing@walmartlabs.com>
2020-01-06 13:21:53 -0800

    Update the Spacing-Page for iOS (#21)

--- src/pages/ios/spacing/index.js
+++ src/pages/ios/spacing/index.js
@@ -1,8 +1,17 @@
 // Libs
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 // Components (from atomic to composite)
-import { Figure, FigureContainer, Paragraph, Heading2, Section, Heading1 } from '@sg/sg-layout'
+import {
+  Paragraph,
+  Heading1,
+  Heading2,
+  Figure,
+  GridItem,
+  Grid,
+  Section,
+} from '@sg/sg-layout'
 
 // Assets
 import examplePNGSource from './images/spacing-structure.png'
@@ -10,35 +19,41 @@ import examplePNGSource from './images/spacing-structure.png'
 import './_style.scss'
 
 
-const IOSSpacingPage = () => (
-  <article className="ios-spacing-page">
+const IOSSpacingPage = () => {
+  const minWidth1024 = useMediaQuery({ query: '(min-width: 1024px)' })
 
-    <Heading1>Spacing</Heading1>
+  return (
+    <article className="ios-spacing-page">
 
-    <Section>
-      <Heading2>Structure</Heading2>
-      <Paragraph>
-        To achieve good visual balance, most measurements align to a 4px scale,
-        with 8px being the preffered standard scale.
-      </Paragraph>
-      <FigureContainer>
-        <Figure noMargin fluid maxWidth="599px" src={examplePNGSource} />
-      </FigureContainer>
-    </Section>
+      <Heading1>Spacing</Heading1>
 
-    <Section>
-      <Heading2>Padding</Heading2>
-      <Paragraph>
-        Padding refers to the space between UI elements. It’s a standard spacing
-        method along with keylines, measured in increments of 8dp (or 4dp in certain cases).
-      </Paragraph>
-      <Paragraph>
-        Padding should be used in combination with the responsive layout grid to
-        place elements consistently across a design.
-      </Paragraph>
-    </Section>
+      <Section>
+        <Heading2>Structure</Heading2>
+        <Paragraph>
+          To achieve good visual balance, most measurements align to a 4px scale,
+          with 8px being the preffered standard scale.
+        </Paragraph>
+        <Grid withBackground>
+          <GridItem>
+            <Figure noMargin fluid maxWidth="599px" src={examplePNGSource} />
+          </GridItem>
+        </Grid>
+      </Section>
 
-  </article>
-)
+      <Section>
+        <Heading2>Padding</Heading2>
+        <Paragraph>
+          Padding refers to the space between UI elements. It’s a standard spacing
+          method along with keylines, measured in increments of 8dp (or 4dp in certain cases).
+        </Paragraph>
+        <Paragraph>
+          Padding should be used in combination with the responsive layout grid to
+          place elements consistently across a design.
+        </Paragraph>
+      </Section>
+
+    </article>
+  )
+}
 
 export default IOSSpacingPage
